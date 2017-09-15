65e541be8760a6afa2cd63613d3e3e1972c6534e
Kevin Joan Correa Del Castillo <kevinjoan16@gmail.com>
2017-09-15 08:58:29 -0500

    feat Se utiliza look and feel del sistema

--- src/aplireportes2/ApliReportes2.java
+++ src/aplireportes2/ApliReportes2.java
@@ -5,6 +5,10 @@
  */
 package aplireportes2;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import views.GUIReporte;
 
 /**
@@ -17,6 +21,26 @@ public class ApliReportes2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            UIManager.LookAndFeelInfo[] lfs = UIManager.getInstalledLookAndFeels();
+            boolean encontro = false;
+            for (UIManager.LookAndFeelInfo lf : lfs) {
+                if(lf.getName().equals("GTK+")){
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+                    encontro = true;
+                }
+            }
+            if(!encontro)
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ApliReportes2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ApliReportes2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ApliReportes2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(ApliReportes2.class.getName()).log(Level.SEVERE, null, ex);
+        }
         GUIReporte gr = new GUIReporte();
         gr.setVisible(true);
     }
